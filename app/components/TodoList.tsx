8691c7598e77647c6db7dfd251e31740f9ce95b4
Eugene <tambov.j678@gmail.com>
2024-12-07 18:07:27 +0300

    remove unused import

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, ReactEventHandler } from "react";
+import React, { useState, useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
